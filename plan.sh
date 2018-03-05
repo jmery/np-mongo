8c14e7a8b14aba1d997fcfab82c646ca82c20338
Jeff Mery <jmery@users.noreply.github.com>
2018-03-05 09:44:15 -0600

    Update pkg_origin & pkg_maintainer

--- plan.sh
+++ plan.sh
@@ -1,8 +1,8 @@
-pkg_origin=echohack
+pkg_origin=jmery
 pkg_name=np-mongodb
 pkg_version=3.2.9
 pkg_description="Mongodb for National-Parks app"
-pkg_maintainer="echohack"
+pkg_maintainer="Jeff Mery <jmery@chef.io>"
 pkg_license=('AGPL-3.0')
 pkg_deps=(core/mongodb core/mongo-tools)
 pkg_svc_run="mongod --config $pkg_svc_config_path/mongod.conf"
